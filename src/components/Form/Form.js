736bf33b25f647e73e26cd74a054425995b6c5b1
agesa3 <collinsagesa2000@gmail.com>
2021-06-16 13:35:35 +0300

    able to POST data

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,6 +3,8 @@ import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from "react-file-base64";
 import { useDispatch } from "react-redux";
 import useStyles from "./styles";
+import { createPost } from "../../actions/posts";
+
 function Form() {
   const [postData, setPostData] = useState({
     creator: "",
@@ -14,9 +16,9 @@ function Form() {
   const classes = useStyles();
   const dispatch = useDispatch();
 
-  const handleSubmit = () => {
-    
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createPost(postData));
   };
   const clear = () => {};
   return (
